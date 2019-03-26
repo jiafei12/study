681c4b5b707ea02b14cd8a6bbd6e877643fa512c
Amber_company <amber@13017232435.com>
2019-03-26 15:39:38 +0800

    ‘初始化http

---
+++ src/main/java/com/demo/util/HttpClientUtils.java
@@ -0,0 +1,21 @@
+package com.demo.util;
+
+/**
+ * httpClient工具类
+ * @author amber
+ * @date Created on 2019年3月26日
+ */
+public class HttpClientUtils {
+
+    private static final String ENCODING = "UTF-8" ;
+
+    private static final int CONNECT_TIMEOUT = 6000 ;
+
+    private static final int SOCKET_TIMEOUT = 6000 ;
+
+
+
+
+
+
+}
